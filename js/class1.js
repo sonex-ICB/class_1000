174aed3ac6f3dd35871904e5b10ba418247cbde9
sonex-ICB <sonex2316@gmail.com>
2026-08-11 13:07:34 +0100

    Classes

---
+++ js/class1.js
@@ -0,0 +1,78 @@
+
+//  Comments
+// output - Print
+console.log("Solomon")
+console.log('Yellow world!')
+console.log(`Welcome to script`)
+
+// variables :
+x = 2
+let y = 10
+const z = x + y
+
+x = 0
+x = 5 
+y = 6
+
+User = 'Solomon'
+user = ""
+full_Name = ''
+user1 = ""
+// console = "" // do not use key words
+
+//  Data type : Number, strin,boolean, array, Object, undefined
+// number  : NUmeric text witout quotes 
+a = 10
+a = -16
+a = 0.5
+a = 0 
+// a =  0998 -error
+
+// strin : Plain text in quotes : An array of xters 
+b = "Solomon"
+b = '14'
+b = '12c'
+
+// Boolean  : a value 'at can only be true || false
+c = true 
+d = false 
+d = 'false' // strin 
+
+let my_file  // undefined
+let my_file1 = undefined // undefined
+
+
+// print a variable from eac' of t'e data type of above 
+console.log(my_file)
+console.log(c)
+console.log(b)
+console.log(a)
+
+// array : List of items 
+fruits = ["Apple", "Water melon",'Pineapple', 'Tomato']
+console.log(fruits)
+console.log(fruits[3])
+console.log(fruits[0])
+mx = [0,7,true, undefined, "Welcome", ['text1','text2']]
+// len = 6, 5
+// print text2
+console.log(mx[5][1]) 
+console.log(mx[1]) 
+
+courses = [
+    ['CSS','B5'],
+    'Script',
+    ['PY','CPP'],
+    ['React']
+]
+// B5
+// Script
+// React
+// CSS
+// PY 
+
+console.log(courses[2][0]) //Py
+console.log(courses[1]) //Script
+console.log(courses[0][1])  //B5
+console.log(courses[0][0])   //CSS
+console.log(courses[3][0])   //react
